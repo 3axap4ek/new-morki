49c61672cea5d725f3ea8817de9989f8912d18f9
3axap <uvanov977777@gmail.com>
2022-01-02 21:40:37 +0300

    det js in slide

--- js/offers-slides.js
+++ js/offers-slides.js
@@ -2,5 +2,13 @@
 
 
 document.addEventListener('DOMContentLoaded', () =>{
+    let slide = document.querySelector('.offers-slide');
+    let i = 0;
+    let slideInterval = setInterval(nextSlide, 2000);
+    
+    function nextSlide(){
+        i += 1%slide.length;
+        slide[i].className = 'offers-slide offers-slide__head--style-reason active-slide';
 
+    }
 });
